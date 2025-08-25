a61318357524270dff82204e92ceb7874da2809c
QuangHuy2006 <tonphamquanghuy3@Gmail.com>
2025-08-25 08:17:56 +0700

    Update App.tsx

--- Session16/src/App.tsx
+++ Session16/src/App.tsx
@@ -4,6 +4,7 @@ import Ex3 from "./components/ex3/Button";
 import Ex4 from "./components/ex4/ClickCounter";
 import Ex5 from "./components/ex5/Validate";
 import Ex6 from "./components/ex6/ChangeTheme";
+import Ex7 from "./components/ex7/ShoppingCart";
 
 function App() {
 
@@ -13,8 +14,9 @@ function App() {
       {/* <Ex2/> */}
       {/* <Ex3/> */}
       {/* <Ex4/> */}
-      <Ex5/>
+      {/* <Ex5/> */}
       {/* <Ex6/> */}
+      <Ex7/>
     </>
   )
 }
